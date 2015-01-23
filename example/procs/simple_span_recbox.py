e7b0ea96121b57d794b039cc7e3ae40cc07f4e60
Michael Chow <machow@princeton.edu>
2015-01-23 16:39:25 -0500

    examples folder

---
+++ example/procs/simple_span_recbox.py
@@ -0,0 +1,12 @@
+from psychopy import visual
+from pandas import DataFrame
+from memtask.proc import ProcShow, RecBox, run_task
+df = DataFrame.from_csv('example/trials/simple_span.csv')
+win = visual.Window([800,600])
+P = ProcShow(win)
+R = RecBox(win, pos = [0, 0])
+proc_dict = {'learn': P,
+             'recall': R
+            }
+
+run_task([row for ii, row in df.iterrows()], proc_dict)
