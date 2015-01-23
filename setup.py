1aafc8933687d075303a3944c95a143490c01b9f
Michael Chow <machow@princeton.edu>
2015-01-23 16:40:04 -0500

    setup.py for package installation

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='memtask',
+      version='0.1',
+      description='psychology tasks programmed for your sanity',
+      url='http://github.com/chowhub/psychopy-tasks',
+      author='Michael Chow',
+      author_email='machow@princeton.edu',
+      packages=['memtask'],
+      install_requires=['psychopy'],
+      zip_safe=False)
+
